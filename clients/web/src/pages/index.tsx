9476889fda2ecc1fdf8527a78f5986570ca71a32
kgserrano <kevgagser@gmail.com>
2021-05-05 14:26:31 -0700

    fix: e2e should work now.

--- clients/web/src/pages/index.tsx
+++ clients/web/src/pages/index.tsx
@@ -2,22 +2,9 @@ import React from 'react'
 import { NextPage } from 'next'
 import { LayoutNavbar } from '@/common/layouts/LayoutNavbar'
 import { AppNavbar } from '@/common/navbar/AppNavbar'
-import { useProjectOwnerQuery } from '../generated/graphql'
-import { Container } from '@chakra-ui/layout'
 
 const Index: NextPage = () => {
-  const { data, loading, error } = useProjectOwnerQuery({
-    variables: { id: '731c1bdc-fb73-45f1-8ba6-7519e97adde4' }
-  })
-
-  return (
-    <LayoutNavbar navbar={<AppNavbar />}>
-      <Container bg="tomato">{JSON.stringify(error)}</Container>
-      <Container>
-        hello, this is {loading ? 'LOADING' : data?.projectOwner?.name}
-      </Container>
-    </LayoutNavbar>
-  )
+  return <LayoutNavbar navbar={<AppNavbar />}>hello</LayoutNavbar>
 }
 
 export default Index
