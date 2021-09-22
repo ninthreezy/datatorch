5370d4b7c49ccd2c40c764bd331da7e713a6bb98
ninthreezy <michaeldnguyen93@gmail.com>
2021-09-22 17:58:59 -0400

    chore: made it so I can merge with master

--- clients/web/src/libs/panels/common/three/utils/ForwardCanvas.tsx
+++ clients/web/src/libs/panels/common/three/utils/ForwardCanvas.tsx
@@ -1,24 +1,23 @@
 import { theme } from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import { Canvas } from 'react-three-fiber'
+import { Canvas, Props } from 'react-three-fiber'
 
 const ThemeProvider: React.FC = ({ children }) => (
   <ChakraProvider theme={theme}>{children}</ChakraProvider>
 )
 
-export const ForwardContext: React.FC<
-  ContainerProps & { ExtraProviders?: React.FC }
-> = ({ ExtraProviders, children, ...props }) => {
-  return (
-    <Canvas {...props}>
-      <ThemeProvider>
-        {ExtraProviders != null ? (
-          <ExtraProviders>{children}</ExtraProviders>
-        ) : (
-          children
-        )}
-      </ThemeProvider>
-    </Canvas>
-  )
-}
+export const ForwardContext: React.FC<Props & { ExtraProviders?: React.FC }> =
+  ({ ExtraProviders, children, ...props }) => {
+    return (
+      <Canvas {...props}>
+        <ThemeProvider>
+          {ExtraProviders != null ? (
+            <ExtraProviders>{children}</ExtraProviders>
+          ) : (
+            children
+          )}
+        </ThemeProvider>
+      </Canvas>
+    )
+  }
